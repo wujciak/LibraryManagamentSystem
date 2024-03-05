8fc1a3f2c219d3f0e24afc0f1aa575259f715ef5
Jakub <jakub.wojciak2002@gmail.com>
2024-03-05 13:14:10 +0100

    Pierwsze laby.

--- src/main/java/edu/ib/technologiesieciowe/HelloController.java
+++ src/main/java/edu/ib/technologiesieciowe/HelloController.java
@@ -5,22 +5,27 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 public class HelloController {
+
     @GetMapping("/helloWorld")
     public String helloWorld() {
         return "Hello world!";
     }
+
     @GetMapping("/helloSomeone")
     public String helloSomeone(@RequestParam(required = false) String name,
                                @RequestParam String surname) {
         if(name == null) name = "John";
         return "Hello " + name + " " + surname + "!";
     }
+
     @GetMapping("/helloPath/{name}")
     public String helloFromPath(@PathVariable String name) {
         return "Hello " + name + "!";
     }
+
     @GetMapping("/addNumbers")
     public double addNumbers(@RequestParam double num1, @RequestParam double num2) {
         return num1 + num2;
     }
+
 }
