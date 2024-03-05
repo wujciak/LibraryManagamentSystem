e668f3f8d3563f6f0094b4258fa0dfbeb2d9751b
Jakub <jakub.wojciak2002@gmail.com>
2024-03-05 13:12:15 +0100

    Pierwsze laby.

---
+++ src/main/java/edu/ib/technologiesieciowe/TechnologieSiecioweApplication.java
@@ -0,0 +1,13 @@
+package edu.ib.technologiesieciowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologieSiecioweApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnologieSiecioweApplication.class, args);
+    }
+
+}
