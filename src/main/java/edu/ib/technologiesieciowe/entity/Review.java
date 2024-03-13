0895e37165b69bdc5b81de338a1d87ca6453ec43
Jakub Wójciak 269321 <jakub.wojciak2002@gmail.com>
2024-03-13 10:27:25 +0100

    Corrected review entity.

--- src/main/java/edu/ib/technologiesieciowe/entity/Review.java
+++ src/main/java/edu/ib/technologiesieciowe/entity/Review.java
@@ -1,11 +1,13 @@
 package edu.ib.technologiesieciowe.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.util.Date;
 
+@Entity
 public class Review {
 
     @Id
